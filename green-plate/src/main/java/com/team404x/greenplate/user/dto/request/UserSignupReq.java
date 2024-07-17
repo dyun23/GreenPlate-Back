7a302f4eda0d39e4fef268d4ea3dd19f052eecd6
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-07-17 12:00:15 +0900

    [Feature] 회원가입 기능 추가 #24

---
+++ green-plate/src/main/java/com/team404x/greenplate/user/dto/request/UserSignupReq.java
@@ -0,0 +1,18 @@
+package com.team404x.greenplate.user.dto.request;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserSignupReq {
+	String email;
+	String password;
+	String name;
+	String nickname;
+	LocalDate birthday;
+}
