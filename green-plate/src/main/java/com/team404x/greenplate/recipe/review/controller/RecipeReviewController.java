58e6e961d2e12e693681d822b06381b33dc8e38a
dyun23 <ozsally@naver.com>
2024-07-18 12:17:33 +0900

    [Feat] 레시피 리뷰 목록 기능 추가(#30)

--- green-plate/src/main/java/com/team404x/greenplate/recipe/review/controller/RecipeReviewController.java
+++ green-plate/src/main/java/com/team404x/greenplate/recipe/review/controller/RecipeReviewController.java
@@ -1,13 +1,13 @@
 package com.team404x.greenplate.recipe.review.controller;
 
 import com.team404x.greenplate.recipe.review.model.request.RecipeReviewCreateReq;
+import com.team404x.greenplate.recipe.review.model.response.RecipeReviewListRes;
 import com.team404x.greenplate.recipe.review.service.RecipeReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/recipe/review")
@@ -18,5 +18,9 @@ public class RecipeReviewController {
     public ResponseEntity<String> createRecipeReview(@RequestBody RecipeReviewCreateReq request) {
         recipeReviewService.saveReview(request);
         return ResponseEntity.ok("Success");
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "/{recipeId}/list")
+    public ResponseEntity<List<RecipeReviewListRes>> getAllReviews(@PathVariable("recipeId") Long recipeId) {
+        return ResponseEntity.ok(recipeReviewService.listReviews(recipeId));
     }
 }
