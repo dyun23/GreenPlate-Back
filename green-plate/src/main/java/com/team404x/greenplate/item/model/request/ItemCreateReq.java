45b7ebf4e120fc9f258b61b60f04340f7daa1efa
mangoade100g <somethingisnothing125@gmail.com>
2024-07-18 15:25:28 +0900

    [Feature] 사업자 상품 등록 기능 구현 #1

---
+++ green-plate/src/main/java/com/team404x/greenplate/item/model/request/ItemCreateReq.java
@@ -0,0 +1,26 @@
+package com.team404x.greenplate.item.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ItemCreateReq {
+   private Long companyId;
+   private String name;
+   private String contents;
+   private int price;
+   private int stock;
+   private int calorie;
+   private String state;
+   private String imageUrl;
+   private int discountPrice;
+   private String mainCategory;
+   private String subCategory;
+
+
+}
