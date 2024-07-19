896a90f434e2ec27c42ee0f0b4678ff0d117dc5a
dyun23 <ozsally@naver.com>
2024-07-19 12:53:02 +0900

    [Feat] 장바구니 목록 조회 및 기타 수정(#84 , #85)

--- green-plate/src/main/java/com/team404x/greenplate/recipe/model/request/RecipeCreateReq.java
+++ green-plate/src/main/java/com/team404x/greenplate/recipe/model/request/RecipeCreateReq.java
@@ -1,13 +1,10 @@
 package com.team404x.greenplate.recipe.model.request;
 
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 @Getter
 public class RecipeCreateReq {
-    private Long companyId;
     private String title;
     private String contents;
     private Long[] itemList;
