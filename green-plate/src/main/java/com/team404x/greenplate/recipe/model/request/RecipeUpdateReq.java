9b0d178be0259ebca5445df7b206f605d16a3591
dyun23 <ozsally@naver.com>
2024-07-19 14:04:30 +0900

    [Fix] 레시피 어노테이션 추가

--- green-plate/src/main/java/com/team404x/greenplate/recipe/model/request/RecipeUpdateReq.java
+++ green-plate/src/main/java/com/team404x/greenplate/recipe/model/request/RecipeUpdateReq.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 @Getter
 public class RecipeUpdateReq {
     private Long recipeId;
-    private Long companyId;
     private String title;
     private String contents;
     private Long[] itemList;
